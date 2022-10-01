8416a7600224c522f3527ca50fbf1d25e220853b
phoebos <ben@bvnf.space>
2022-10-01 22:28:27 +0100

    sleep: exit normally on SIGALRM

--- sleep.c
+++ sleep.c
@@ -1,7 +1,14 @@
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void
+sig_handler(int sig) {
+    (void)sig;
+    exit(0);
+}
 
 int
 main(int argc, char **argv) {
@@ -9,6 +16,8 @@ main(int argc, char **argv) {
         fprintf(stderr, "usage: %s time\n", *argv);
         return 1;
     }
+
+    signal(SIGALRM, sig_handler);
 
     errno = 0;
     unsigned long t = strtoul(argv[1], NULL, 10);
